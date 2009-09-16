2414cc0896fd48020e7dc346e772692aa53d2b64
dit06ajn <dit06ajn@1d825168-7144-41e2-cc63-fee83f03a819>
2009-09-16 14:49:48 +0000

    Start a test

--- start-client.sh
+++ start-client.sh
@@ -1,2 +1,2 @@
 #! /usr/bin/env bash
-java -cp bin/main/java/ -Djava.rmi.server.codebase=file:bin/main/java/ se.umu.cs.jsgajn.gcom.Client
+java -cp bin/main/java/:lib/log4j-1.2.15.jar -Djava.rmi.server.codebase=file:bin/main/java/ se.umu.cs.jsgajn.gcom.testapp.ChatMember $@
