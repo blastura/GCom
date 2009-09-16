4882603e738fd96fcd7c3dd3e74c829fdf2d3b6b
dit06ajn <dit06ajn@1d825168-7144-41e2-cc63-fee83f03a819>
2009-09-16 13:46:51 +0000

    Removed shitty files

--- src/main/java/se/umu/cs/jsgajn/gcom/ChatServer.java
+++
@@ -1,8 +0,0 @@
-package se.umu.cs.jsgajn.gcom;
-
-public interface ChatServer {
-	// List<Client> clients; 	
-	public void connect("Client");
-	public void say("String");
-	// When someone says something send to other clients;
-}
