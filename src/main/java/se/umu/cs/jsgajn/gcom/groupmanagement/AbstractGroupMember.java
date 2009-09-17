ed758e6640b29b732ca04f553ad93c0203cfbe00
dit06ajn <dit06ajn@1d825168-7144-41e2-cc63-fee83f03a819>
2009-09-17 09:44:51 +0000

    Formatting

--- src/main/java/se/umu/cs/jsgajn/gcom/groupmanagement/AbstractGroupMember.java
+++ src/main/java/se/umu/cs/jsgajn/gcom/groupmanagement/AbstractGroupMember.java
@@ -16,24 +16,17 @@ public abstract class AbstractGroupMember implements GroupMember {
     private GroupLeader groupLeader;
 
     /**
-     * @param gnsHost
-     *            GNS host
-     * @param gnsPort
-     *            GNS port
-     * @param groupName
-     *            Group name to connect to
-     * @throws RemoteException
-     *             If GNS throws exception
-     * @throws AlreadyBoundException
-     *             If it's not possible to bind this to own registry.
-     * @throws NotBoundException
-     *             If GNS stub is not found in GNS register.
+     * @param gnsHost GNS host
+     * @param gnsPort GNS port
+     * @param groupName Group name to connect to
+     * @throws RemoteException If GNS throws exception
+     * @throws AlreadyBoundException If it's not possible to bind this to own register.
+     * @throws NotBoundException If GNS stub is not found in GNS register.
      */
     public AbstractGroupMember(String gnsHost, int gnsPort, String groupName)
             throws RemoteException, AlreadyBoundException, NotBoundException {
         this.stub = (GroupMember) UnicastRemoteObject.exportObject(this, 0);
-        Registry registry = LocateRegistry.createRegistry(1099); // TODO: change
-                                                                 // 1099
+        Registry registry = LocateRegistry.createRegistry(1099); // TODO: change 1099
         registry.bind(GroupMember.STUB_NAME, stub);
         this.gns = connectToGns(gnsHost, gnsPort);
         this.groupLeader = gns.connect(stub, groupName);
@@ -41,15 +34,11 @@ public abstract class AbstractGroupMember implements GroupMember {
     }
 
     /**
-     * @param host
-     *            Host to GNS
-     * @param port
-     *            Port to GNS
+     * @param host Host to GNS
+     * @param port Port to GNS
      * @return GNS stub
-     * @throws RemoteException
-     *             If GNS throws exception
-     * @throws NotBoundException
-     *             If GNS stub can't be found.
+     * @throws RemoteException If GNS throws exception
+     * @throws NotBoundException If GNS stub can't be found.
      */
     private GNS connectToGns(String host, int port) throws RemoteException,
             NotBoundException {
