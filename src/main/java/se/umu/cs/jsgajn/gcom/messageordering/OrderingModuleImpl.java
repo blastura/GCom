ccb291b5293f418da121d80de17f752364f3e790
dit06ajn <dit06ajn@1d825168-7144-41e2-cc63-fee83f03a819>
2009-10-02 09:03:12 +0000

    rename

---
+++ src/main/java/se/umu/cs/jsgajn/gcom/messageordering/OrderingModuleImpl.java
@@ -0,0 +1,55 @@
+package se.umu.cs.jsgajn.gcom.messageordering;
+
+import se.umu.cs.jsgajn.gcom.Module;
+import se.umu.cs.jsgajn.gcom.groupcommunication.Message;
+import se.umu.cs.jsgajn.gcom.groupmanagement.GroupView;
+
+/**
+ * Module to handle different orderings of messages.
+ *
+ * @author dit06ajn, dit06jsg
+ */
+public class OrderingModuleImpl implements Module {
+    private Ordering ordering;
+    private Module communicationsModule;
+    private Module groupManagementModule;
+
+    public OrderingModuleImpl(Module groupManagementModule,
+                              Ordering ordering) {
+        this.groupManagementModule = groupManagementModule;
+        this.ordering = ordering;
+
+        new Thread(new DeliverHandler()).start();
+    }
+
+    public void setCommunicationsModule(Module m) {
+        this.communicationsModule = m;
+    }
+
+    public void send(Message m, GroupView g) {
+        communicationsModule.send(ordering.prepareOutgoingMessage(m), g);
+    }
+
+    public void deliver(Message m) {
+        ordering.put(m);
+    }
+
+    private class DeliverHandler implements Runnable {
+        public void run() {
+            while (true) {
+                groupManagementModule.deliver(ordering.take());
+            }
+        }
+    }
+
+    public void setDeliverReceiver(Module m) {
+        // TODO Auto-generated method stub
+        throw new Error("TODO: fix");
+
+    }
+
+    public void setSendReceiver(Module m) {
+        // TODO Auto-generated method stub
+        this.communicationsModule = m;
+    }
+}
