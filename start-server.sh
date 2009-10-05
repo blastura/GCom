3c6604214e89820ac486e16cf170dee05dd6175b
dit06ajn <dit06ajn@1d825168-7144-41e2-cc63-fee83f03a819>
2009-10-05 12:41:19 +0000

    fixed class path in scripts

--- start-server.sh
+++ start-server.sh
@@ -1,2 +1,6 @@
 #! /usr/bin/env bash
-java -cp bin/main/java/:lib/log4j-1.2.15.jar -Djava.rmi.server.codebase=file:bin/main/java/ se.umu.cs.jsgajn.gcom.groupmanagement.GNSImpl $@
+# (mapconcat '(lambda (path)
+#               (car (cdr (split-string path default-directory))))
+#             (my-get-classpath) ":")
+cp=bin/main/java:bin/test/java:lib/core.jar:lib/junit-4.5.jar:lib/logback-classic-0.9.17.jar:lib/logback-core-0.9.17.jar:lib/slf4j-api-1.5.8.jar
+java -cp $cp -Djava.rmi.server.codebase=file:bin/main/java/ se.umu.cs.jsgajn.gcom.groupmanagement.GNSImpl $@
