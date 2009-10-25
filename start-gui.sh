3004556e87abd8c27f82f5ee9ecb90b384a096dd
dit06ajn <dit06ajn@1d825168-7144-41e2-cc63-fee83f03a819>
2009-10-25 07:04:13 +0000

    Start  gui script

---
+++ start-gui.sh
@@ -0,0 +1,7 @@
+#! /usr/bin/env bash
+# Elisp to create classpath
+# (mapconcat '(lambda (path)
+#              (car (cdr (split-string path default-directory))))
+#            (my-get-classpath) ":")
+cp="bin/main/java:bin/test/java:lib/core.jar:lib/junit-4.5.jar:lib/logback-classic-0.9.17.jar:lib/logback-core-0.9.17.jar:lib/slf4j-api-1.5.8.jar"
+java -cp $cp -Djava.rmi.server.codebase=file:bin/main/java/ se.umu.cs.jsgajn.gcom.testapp.TestApp $@
