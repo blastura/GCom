ffd38fec42a9d7bcf2bbb74e40ca5e1c41a77617
dit06ajn <dit06ajn@1d825168-7144-41e2-cc63-fee83f03a819>
2009-10-27 12:15:24 +0000

    awway with that

--- src/main/java/se/umu/cs/jsgajn/gcom/management/GroupSettings.java
+++ src/main/java/se/umu/cs/jsgajn/gcom/management/GroupSettings.java
@@ -5,6 +5,11 @@ import java.io.Serializable;
 import se.umu.cs.jsgajn.gcom.communication.MulticastType;
 import se.umu.cs.jsgajn.gcom.ordering.OrderingType;
 
+/**
+ * Class to represent different settings for a group.
+ *
+ * @author dit06ajn, dit06jsg
+ */
 public class GroupSettings implements Serializable {
     private static final long serialVersionUID = 1L;
     private GroupMember leader;
@@ -12,7 +17,15 @@ public class GroupSettings implements Serializable {
     private OrderingType orderingType;
     private boolean isNew = true;
     private String name;
-
+    
+    /**
+     * Creates a new instance of groups settings.
+     *
+     * @param name The name of the group.
+     * @param leader The leader of the group.
+     * @param multicastType the type of multicast to use.
+     * @param orderingType the type of ordering to use.
+     */
     public GroupSettings(String name, GroupMember leader,
                          MulticastType multicastType,
                          OrderingType orderingType) {
@@ -22,26 +35,45 @@ public class GroupSettings implements Serializable {
         this.orderingType = orderingType;
     }
 
+    /**
+     * @return The name of the group.
+     */
     public String getName() {
         return this.name;
     }
 
+    /**
+     * @param isNew Set if the group newly created, this is used by
+     * the GNS.
+     */
     public void setIsNew(boolean isNew) {
         this.isNew = isNew;
     }
 
+    /**
+     * @return true if the group is just created by the GNS.
+     */
     public boolean isNew() {
         return isNew;
     }
 
+    /**
+     * @return The group leader.
+     */
     public GroupMember getLeader() {
         return leader;
     }
 
+    /**
+     * @return The multicast type.
+     */
     public MulticastType getMulticastType() {
         return multicastType;
     }
 
+    /**
+     * @return The ordering type.
+     */
     public OrderingType getOrderingType() {
         return orderingType;
     }
@@ -53,7 +85,10 @@ public class GroupSettings implements Serializable {
             + this.getOrderingType() + "]";
     }
 
-	public void setLeader(GroupMember gm) {
-		this.leader = gm;
-	}
+    /**
+     * @param gm Set a new group leader.
+     */
+    public void setLeader(GroupMember gm) {
+        this.leader = gm;
+    }
 }
